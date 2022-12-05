0311d997038725dae9a8c57d32dafb25d7a369b3
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-05 21:33:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,21 +1,19 @@
-# include <iostream>
+
 # include "lib.h"
 using namespace std;
 bool cosa(char &a)
 {
-    if (((int) a >= 65 && (int) a <= 90) or ((int) a >= 97 && (int) a <= 122))
+    if (65<=a && a<90)
     {
-        if ((int) a >= 65 && (int) a <= 90) 
-        {
-            a = a + 32;
-        }
-        else
-        {
-            a = a - 32;
-        }
+        a=a+32;
         return true;
     }
-    else
+    else if (97<=a && a<=122)
+    {
+        a=a-32;
+        return true;
+    }
+    else 
     {
         return false;
     }
