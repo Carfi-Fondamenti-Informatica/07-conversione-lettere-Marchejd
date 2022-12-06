da47f6c47b147778574e550683648de5b0f020f7
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-06 23:21:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-
+#include <iostream>
 #include "lib.h"
 using namespace std;
 int main() {
     char n=0;
     cin>>n;
     if (lettera(n)==true)
-        cout<< n <<endl;;
+        cout<< n <<endl;
     else
         cout<<"errore"<<endl;
     return 0;
