e384148ee904fade66b93d0ccbadc550708195cf
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-06 23:24:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,21 +1,22 @@
 
+#include <iostream>
 #include "lib.h"
-bool lettera(char &a)
-{
-    if ((a >= 65 &&  a <= 90) or (a >= 97 && a <= 122))
+
+using namespace std;
+
+bool lettera(char &a){
+    if (a >=65 && a <=90 )
     {
-        if ( a >= 65 &&  a <= 90) {
-            a = a + 32;
-        }
-        else
-        {
-            a = a - 32;
-        }
+        a+= 32;
+        return true;
+    }
+    else if(a >=97 && a<=122 )
+    {
+        a-=32;
         return true;
     }
     else
-    {
-        return false;
-    }
+       return false;
+    
 
 }
